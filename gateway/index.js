e74ddabd0af4e84543a6b5eeac4f7b510cb0e12f
Lukasz Lato <lukasz.lato@summer-solutions.com>
2020-10-29 14:30:24 +0100

    gateway

--- gateway/index.js
+++ gateway/index.js
@@ -3,15 +3,14 @@ const { ApolloGateway } = require("@apollo/gateway");
 
 const gateway = new ApolloGateway({
     serviceList: [
-        { name: 'accounts', url: 'http://localhost:4001/query' },
-        { name: 'products', url: 'http://localhost:4002/query' },
-        { name: 'reviews', url: 'http://localhost:4003/query' }
+        { name: 'accounts', url: 'https://accounts.supper-api.com/query' },
+        { name: 'products', url: 'https://products.supper-api.com/query' },
+        { name: 'reviews', url: 'https://reviews.supper-api.com/query' }
     ],
 });
 
 const server = new ApolloServer({
     gateway,
-
     subscriptions: false,
 });
 
