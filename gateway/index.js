9d8a3d33c2f67e7fc0c09c083c8d6c1092db9c89
Lukasz Lato <lukasz.lato@summer-solutions.com>
2020-10-29 14:57:46 +0100

    gateway

--- gateway/index.js
+++ gateway/index.js
@@ -1,19 +1,23 @@
-const { ApolloServer } = require('apollo-server');
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloServer } = require('apollo-server')
+const { ApolloGateway } = require('@apollo/gateway')
 
 const gateway = new ApolloGateway({
     serviceList: [
         { name: 'accounts', url: 'https://accounts.supper-api.com/query' },
         { name: 'products', url: 'https://products.supper-api.com/query' },
-        { name: 'reviews', url: 'https://reviews.supper-api.com/query' }
+        { name: 'reviews', url: 'https://reviews.supper-api.com/query' },
     ],
-});
+})
 
 const server = new ApolloServer({
     gateway,
     subscriptions: false,
-});
+})
 
-server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-});
+server
+    .listen({
+        port: process.env.PORT || 8080,
+    })
+    .then(({ url }) => {
+        console.log(`🚀 Server ready at ${url}`)
+    })
