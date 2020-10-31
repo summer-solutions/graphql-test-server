7894fab5d51c1b3f47a86b5bff626894bbc51156
Lukasz Lato <lukasz.lato@summer-solutions.com>
2020-10-31 19:33:08 +0100

    test errors

--- internal/server/server.go
+++ internal/server/server.go
@@ -50,29 +50,13 @@ func graphqlHandler(server graphql.ExecutableSchema) gin.HandlerFunc {
 	h := handler.NewDefaultServer(server)
 	h.SetRecoverFunc(func(ctx context.Context, err interface{}) error {
 		request := GinContextFromContext(ctx)
-		requestPart := gin.H{"requestMethod": request.Request.Method,
-			"requestUrl":   "https://accounts.summer-api.com/query",
-			"requestSize":  "399",
-			"responseSize": "347",
-			"serverIp":     "74.125.133.121",
-			"latency":      "0.005101409s",
-			"protocol":     "HTTP/1.1",
-			"userAgent":    request.Request.UserAgent(),
-			"referrer":     request.Request.Referer(),
-			"status":       200,
-			"remoteIp":     "35.203.251.75"}
-		l := log.WithField("httpRequest", requestPart)
-		l = l.WithField("user", "llato")
-		l = l.WithField("@type", "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent")
-		l = l.WithField("serviceContext", gin.H{"service": "frontend", "version": "bf6b5b09b9d3da92c7bf964ab1664fe751104517"})
-
 		var trace string
 		traceHeader := request.Request.Header.Get("X-Cloud-Trace-Context")
 		traceParts := strings.Split(traceHeader, "/")
 		if len(traceParts) > 0 && len(traceParts[0]) > 0 {
 			trace = fmt.Sprintf("projects/%s/traces/%s", os.Getenv("GC_PROJECT_ID"), traceParts[0])
 		}
-		l = l.WithField("logging.googleapis.com/trace", trace)
+		l := log.WithField("logging.googleapis.com/trace", trace)
 		var message string
 		asErr, is := err.(error)
 		if is {
@@ -80,7 +64,8 @@ func graphqlHandler(server graphql.ExecutableSchema) gin.HandlerFunc {
 		} else {
 			message = "panic"
 		}
-		l.Error(message + "\n" + string(debug.Stack()))
+		stack := strings.Split(string(debug.Stack()), "\n")
+		l.Error(message + "\n" + strings.Join(stack[7:], "\n"))
 		return errors.New("internal server error")
 	})
 	return func(c *gin.Context) {
