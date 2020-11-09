ccb8174d8e34dda926852daa553f6db674a43538
Lukasz Lato <lukasz.lato@summer-solutions.com>
2020-11-09 20:17:03 +0100

    test error

--- services/accounts/graph/schema.resolvers.go
+++ services/accounts/graph/schema.resolvers.go
@@ -7,11 +7,9 @@ import (
 	"context"
 	"summer-solutions/graphql-test-server/services/accounts/graph/generated"
 	"summer-solutions/graphql-test-server/services/accounts/graph/model"
-	"time"
 )
 
 func (r *queryResolver) Me(ctx context.Context) (*model.User, error) {
-	ctx.Value("sss").(time.Time).UnixNano()
 	return &model.User{
 		ID:       "1234",
 		Username: "Me",
